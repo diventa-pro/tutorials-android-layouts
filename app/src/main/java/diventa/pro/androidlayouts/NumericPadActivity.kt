12df34ed5e2d5e9d12c3d99ea7073d92e9c3c790
Daniele Demichelis <d.demichelis@techgap.it>
2018-07-11 17:37:03 +0200

    First NumPad layout with ConstriantLayout

---
+++ app/src/main/java/diventa/pro/androidlayouts/NumericPadActivity.kt
@@ -0,0 +1,12 @@
+package diventa.pro.androidlayouts
+
+import android.app.Activity
+import android.os.Bundle
+
+class NumericPadActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numeric_pad)
+    }
+}
